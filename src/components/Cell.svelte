94f4c28f70a4d52c63700db10dc234e3de947451
nbert71 <nicobert2205@gmail.com>
2022-09-15 00:34:08 +0200

    💄 keyframes

--- src/components/Cell.svelte
+++ src/components/Cell.svelte
@@ -18,7 +18,44 @@
 </script>
 
 <style>
-    .revealed {@apply bg-leaf;}
-    .finded { @apply bg-gradient-to-b from-white via-orange-400 to-yellow-300;}
+    @keyframes reveal {
+        from {
+            transform: scale(1);
+        }
+
+        50% {
+            transform: scale(1.4);
+        }
+
+        to {
+            transform: scale(1);
+        }
+    }
+
+    @keyframes find {
+        from {
+            transform: scale(1);
+        }
+
+        50% {
+            transform: scale(1.4);
+        }
+
+        to {
+            transform: scale(1);
+        }
+    }
+
+    .revealed {
+        @apply bg-green-600 text-white;
+        animation-name: reveal;
+        animation-duration: 0.5s;
+
+    }
+    .finded { 
+        @apply bg-gradient-to-b from-white via-orange-400 to-yellow-300;
+        animation-name: find;
+        animation-duration: 0.5s;
+    }
     .blank {@apply bg-gray-400;}
 </style>
--- src/components/Letter.svelte
+++ src/components/Letter.svelte
@@ -2,19 +2,27 @@
     export let letter;
     export let displayed;
     export let list;
+    let enlarge = false
 
     function reveal() {
         displayed = true;
         list.discover_letter(letter);
         list = list;
+        enlarge = true
+        setTimeout( () => enlarge = false, 200);
     }
 </script>
 
 <button on:click={reveal} class="w-full aspect-square cursor-pointer flex items-center justify-center text-5xl
-    border border-black rounded-md bg-white shadow-lg hover:bg-gray-100 hover:scale-110 transition-all">
+    border border-black rounded-md bg-white shadow-lg hover:bg-gray-100 transition-all"
+    class:scale-letter={enlarge}>
     {#if displayed}
         {letter}
     {:else}
         ?
     {/if}
 </button>
+
+<style>
+    .scale-letter { @apply scale-125;}
+</style>
