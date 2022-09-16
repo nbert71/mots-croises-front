07596dec298c16ef40f838fbadf0dc600b72f77a
nbert71 <nicobert2205@gmail.com>
2022-09-16 16:52:28 +0200

    ✨ mise en place du router + login page

--- src/App.svelte
+++ src/App.svelte
@@ -1,56 +1,17 @@
-<Navbar />
-
-<div class="flex w-4/5 mt-10">
-    <div class="w-1/3 flex flex-col items-center">
-        <MyLetters bind:letters={letters} bind:list={list}/>
-    </div>  
-    <div class="w-2/3 flex flex-col items-center justify-center">
-        <Grid {list} {letters}/>
-    </div> 
-</div>
-
+<Router {routes} />
 
 <script>
-    import Navbar from "./components/Navbar.svelte";
-    import MyLetters from "./components/MyLetters.svelte";
-    import Grid from "./components/Grid.svelte";
-    import {GridJS} from "./components/GridJS.js";
+    import Router from 'svelte-spa-router'
 
-    let list = new GridJS([
-        "--CHIC--NEZ",
-        "---O-APRE--",
-        "--CUIR--T-E",
-        "M--X-ARC--M",
-        "A-M--T-A--E",
-        "T-E--SAPHIR",
-        "IODES--I--A",
-        "N-I--U-TENU",
-        "E-AGENDA--D",
-        "E----I-L--E",
-        "SOUDE------",
-    ])
-    list.find_words()
+    import Game from "./routes/Game.svelte";
+    import Login from "./routes/Login.svelte";
+    import NotFound from "./routes/NotFound.svelte";
 
-    let letters = list.generate_my_letters()
 
-    //let letters_size = 14;
-    // let letters = {
-    //     list:['A','B','C','D','E','F','G','H','I','J','K','L','M','N'],
-    //     displayed:{
-    //         A:false,
-    //         B:false,
-    //         C:false,
-    //         D:false,
-    //         E:false,
-    //         F:false,
-    //         G:false,
-    //         H:false,
-    //         I:false,
-    //         J:false,
-    //         K:false,
-    //         L:false,
-    //         M:false,
-    //         N:false
-    //     }
-    // }
+    const routes = {
+        '/': Game,
+        '/login': Login,
+        '*': NotFound
+    }
+
 </script>
---
+++ src/routes/Login.svelte
@@ -0,0 +1,35 @@
+<div class="flex h-screen flex-col justify-center py-12 sm:px-6 lg:px-8"> 
+    <div class="sm:mx-auto sm:w-full sm:max-w-md">
+      <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+        <h1 class="font-bold mb-4 text-center text-xl">Se connecter</h1>
+        <form class="space-y-6" action="#" method="POST">
+          <div>
+            <div class="mt-1">
+              <input id="username" name="username" type="text" required
+              placeholder="Nom d'utilisateur"
+              class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400
+              shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm">
+            </div>
+          </div>
+  
+          <div>
+            <div class="mt-1">
+              <input id="password" name="password" type="password" autocomplete="current-password" required
+              placeholder="Mot de passe"
+              class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400
+              shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm">
+            </div>
+          </div>
+
+          <div>
+            <button type="submit" class="flex w-full justify-center rounded-md border border-transparent bg-blue-600
+            py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2
+            focus:ring-blue-500 focus:ring-offset-1">
+                Sign in
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+</div>
+  
