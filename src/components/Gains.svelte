b3171e453ae10275829afb945443e006a3854299
Jean-Baptiste-DP <jb.durandpierre@gmail.com>
2022-09-22 11:01:48 +0200

    💄 style de la barre de gains

--- src/components/Gains.svelte
+++ src/components/Gains.svelte
@@ -1,13 +1,25 @@
-<div class="w-1/4 bg-red-300">
-    <h2>Username</h2>
-    <p>9 mots : 40 000€</p>
-    <p>8 mots : 1 000€</p>
-    <p>7 mots : 500€</p>
-    <p>6 mots : 100€</p>
-    <p>5 mots : 30€</p>
-    <p>4 mots : 15€</p>
-    <p>3 mots : 6€</p>
-    <p>2 mots : 3€</p>
-    <p>1 mots : 0€</p>
-    <p>0 mots : 0€</p>
+<div class="w-1/4 bg-yellow-100 h-4/5">
+    <h2 class="text-center text-6xl my-5">Username</h2>
+    <div class="relative h-full">
+        <p class="text-6xl text-center my-2">9 mots : <span class="inline font-black text-amber-600">40 000€</span></p>
+        <p class="text-5xl text-center my-2">8 mots : <span class="inline font-black text-amber-600">1 000€</span></p>
+        <p class="text-5xl text-center my-2">7 mots : <span class="inline font-black text-amber-600">500€</span></p>
+        <p class="text-4xl text-center my-2">6 mots : <span class="inline font-black text-amber-600">100€</span></p>
+        <p class="text-4xl text-center my-2">5 mots : <span class="inline font-black text-amber-600">30€</span></p>
+        <p class="text-3xl text-center my-2">4 mots : <span class="inline font-black text-amber-600">15€</span></p>
+        <p class="text-3xl text-center my-2">3 mots : <span class="inline font-black text-amber-600">6€</span></p>
+        <p class="text-2xl text-center my-2">2 mots : <span class="inline font-black text-amber-600">3€</span></p>
+        <p class="text-2xl text-center my-2">1 mot : <span class="inline font-black text-amber-600">0€</span></p>
+        <p class="text-2xl text-center my-2">0 mots : <span class="inline font-black text-amber-600">0€</span></p>
+        <div class="absolute z-10 top-0 bottom-[30%] couleur-cache w-full"></div>
+        <div class="absolute z-10 bottom-[75%] top-[70%] w-full"></div>
+        <div class="absolute z-10 bottom-0 top-[75%] couleur-cache w-full"></div>
+    </div>
 </div>
+
+<style>
+    .couleur-cache{
+        background-color: rgba(250,204,21,0.8);
+    }
+
+</style>
