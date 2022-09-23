9d20c9268f76566d9e9ff6835983409ee976b4e4
nbert71 <nicobert2205@gmail.com>
2022-09-23 17:33:12 +0200

    💄 style gains avec animations

--- src/components/Gains.svelte
+++ src/components/Gains.svelte
@@ -1,35 +1,67 @@
 <div class="w-1/4 bg-amber-100 h-min">
-    <h2 class="text-center text-4xl py-5 bg-amber-300">Tableau des gains</h2>
-    <div class="relative h-full">
-        <p class="text-6xl echelon" class:current={finded == 9}>9 mots : <span class="money">40 000€</span></p>
-        <p class="text-5xl echelon" class:current={finded == 8}>8 mots : <span class="money">1 000€</span></p>
-        <p class="text-5xl echelon" class:current={finded == 7}>7 mots : <span class="money">500€</span></p>
-        <p class="text-4xl echelon" class:current={finded == 6}>6 mots : <span class="money">100€</span></p>
-        <p class="text-4xl echelon" class:current={finded == 5}>5 mots : <span class="money">30€</span></p>
-        <p class="text-3xl echelon" class:current={finded == 4}>4 mots : <span class="money">15€</span></p>
-        <p class="text-3xl echelon" class:current={finded == 3}>3 mots : <span class="money">6€</span></p>
-        <p class="text-2xl echelon" class:current={finded == 2}>2 mots : <span class="money">3€</span></p>
-        <p class="text-2xl echelon" class:current={finded == 1}>1 mot : <span class="money">0€</span></p>
-        <p class="text-2xl echelon" class:current={finded == 0}>0 mot : <span class="money">0€</span></p>
-        <!-- <div class="absolute z-10 top-0 bottom-[30%] couleur-cache w-full"></div>
-        <div class="absolute z-10 bottom-[75%] top-[70%] w-full"></div>
-        <div class="absolute z-10 bottom-0 top-[75%] couleur-cache w-full"></div> -->
+    <h2 class="text-center text-4xl py-5 bg-amber-300">Tableau des gains {topHeight}</h2>
+    <div class="relative h-full"> 
+        <div class="gain">
+            <p>9 mots : <span class="money">40 000€</span></p>
+        </div>
+        <div class="gain">
+            <p>8 mots : <span class="money">1 000€</span></p>
+        </div>
+        <div class="gain">
+            <p>7 mots : <span class="money">5000€</span></p>
+        </div>
+        <div class="gain">
+            <p>6 mots : <span class="money">100€</span></p>
+        </div>
+        <div class="gain">
+            <p>5 mots : <span class="money">30€</span></p>
+        </div>
+        <div class="gain">
+            <p>4 mots : <span class="money">15€</span></p>
+        </div>
+        <div class="gain">
+            <p>3 mots : <span class="money">6€</span></p>
+        </div>
+        <div class="gain">
+            <p>2 mots : <span class="money">3€</span></p>
+        </div>
+        <div class="gain">
+            <p>1 mot : <span class="money">0€</span></p>
+        </div>
+        <div class="gain">
+            <p>0 mot : <span class="money">0€</span></p>
+        </div>
+
+        <div class="cover top-0 top-cover" style="--top-height: {topHeight}"></div>
+        <div class="cover bottom-0 bottom-cover" style="--bottom-height: {bottomHeight}"></div>
     </div>
 </div>
 
 <script>
     export let finded;
+
+    function scale (number, inMin, inMax, outMin, outMax) {
+        return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    };
+
+    // finded varie de 0 à 9 (les cas ou on a 10 ou 11 mots sont à traiter)
+    //il y a donc 10 gains possibles
+    // ex: finded = 2 --> div du haut = 70% // div du bas = 20%
+    // la somme doit toujours faire 90%, on laisse 10% pour afficher le gain en cours
+    $: topHeight = scale(finded, 0, 9, 90, 0)+'%';
+    $: bottomHeight = scale(finded, 0, 9, 0, 90)+'%';
 </script>
 
 <style>
+    .gain { @apply h-12 bg-yellow-100 flex items-center justify-center text-2xl;}
     .money { @apply inline font-black text-amber-600;}
-    .echelon { @apply text-center py-2;}
 
-    .current { @apply bg-amber-300 transition-all duration-500;}
-    /* .current>.money{ @apply text-white;} */
-
-    .couleur-cache{
-        background-color: rgba(250,204,21,0.8);
+    .cover { @apply absolute z-10 bg-yellow-300/[0.6] w-full transition-all duration-1000;}
+    .top-cover {
+        height: var(--top-height);
+    }
+    .bottom-cover {
+        height: var(--bottom-height);
     }
 
 </style>
