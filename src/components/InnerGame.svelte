c94391fb98c201742ccab6b0f204de6416c62671
Jean-Baptiste-DP <jbdurand@gmail.com>
2023-01-23 13:47:53 +0100

    🚀 end dynamic grid

---
+++ src/components/InnerGame.svelte
@@ -0,0 +1,32 @@
+
+<div class="flex justify-evenly items-center w-full h-full my-10">
+    <MyLetters bind:letters={letters} bind:list={list}/>
+    <Grid {list} {letters}/>
+    <Gains {finded}/>
+</div>
+
+
+
+
+<script>
+    import MyLetters from "../components/MyLetters.svelte";
+    import Grid from "../components/Grid.svelte";
+    import Gains from "../components/Gains.svelte";
+    import { GridJS } from "./GridJS";
+
+    export let response;
+    let list = new GridJS(response.grid);
+    list.find_words();
+    let letters = {
+        list : response.displayedLetters,
+        displayed : {}
+    };
+
+    for(let i=0; i<letters.list.length;i++){
+        letters.displayed[letters.list[i]]=false
+    }
+
+    $: finded = list.finded_words
+
+    console.log(response)
+</script>
--- src/routes/Game.svelte
+++ src/routes/Game.svelte
@@ -5,45 +5,19 @@
 
 <Navbar opacity={70}/>
 
-<div class="flex justify-evenly items-center w-full h-full my-10">
-    
-    <!-- {#if finded >= 0}
-        <MyLetters bind:letters={letters} bind:list={list}/>
-        <Grid {list} {letters}/>
-        <Gains {finded}/>
-    {:else}
-        <p>loading</p>
-    {/if} -->
+{#await fetchURL('GET', '/game/new')}
+    <p>Loading</p>
+{:then response} 
+    <InnerGame {response}/>
+{/await}
 
-    {#await getGame()}
-        loading...
-    {:then ret}
-        <!-- <MyLetters bind:letters={ret.letters} bind:list={ret.list}/>
-        <Grid list={ret.list} letters={ret.letters}/> -->
-    {/await}
-
-
-</div>
 
 
 <script>
     import Navbar from "../components/Navbar.svelte";
-    import MyLetters from "../components/MyLetters.svelte";
-    import Grid from "../components/Grid.svelte";
-    import Gains from "../components/Gains.svelte";
+    import InnerGame from "../components/InnerGame.svelte";
+    
     import {GridJS} from "../components/GridJS.js";
     import { fetchURL } from "./../api";
-
-    async function getGame(){
-        console.log('coucou')
-        let resp = await fetchURL('GET', '/game/new')
-        let ret = {
-            list: new GridJS(resp.grid),
-            letters: resp.displayedLetters
-        }
-        ret.list.find_words()
-        console.log(ret)
-        return ret
-    }
 
 </script>
